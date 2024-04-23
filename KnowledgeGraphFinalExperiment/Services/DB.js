13ab4f8470ddfad4740ec706bffc1fb63717ea16
Hannah Hely <97140666+helyhan@users.noreply.github.com>
2024-04-23 11:15:26 +1000

    Update DB.js
    updated comments

--- KnowledgeGraphFinalExperiment/Services/DB.js
+++ KnowledgeGraphFinalExperiment/Services/DB.js
@@ -1,10 +1,26 @@
 import neo4j from 'neo4j-driver';
+
+/**
+ * This class provides a static method to interact with a Neo4j database.
+ * It handles the connection, execution, and closing of queries securely.
+*/
 export default class DB
 {
-    static URL = 'XXX'
+    // The URL of the Neo4j database
+    static URL = 'XXX' 
+    // Username for database authentication
     static USER = 'XXX'
+    // Password for database authentication
     static PASSWORD = 'XXX'
 
+    /**
+     * Executes a given Cypher command on the Neo4j database with the specified parameters.
+     * @param {string} command - The Cypher query to execute.
+     * @param {Object} params - An object containing the parameters for the query.
+     * @returns {Promise<neo4j.Result>} - A promise that resolves with the result of the query.
+     * This method ensures that the session and driver are properly closed after the operation,
+     * regardless of whether it succeeds or fails.
+    */
     static async run(command, params)
     {
         const driver = neo4j.driver(DB.URL, neo4j.auth.basic(DB.USER, DB.PASSWORD))
