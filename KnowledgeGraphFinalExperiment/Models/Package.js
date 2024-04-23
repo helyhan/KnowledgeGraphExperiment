7ed6b0be15a2a90858d7409719a81988c3bdb601
Hannah Hely <97140666+helyhan@users.noreply.github.com>
2024-04-23 11:13:19 +1000

    Update Package.js

--- KnowledgeGraphFinalExperiment/Models/Package.js
+++ KnowledgeGraphFinalExperiment/Models/Package.js
@@ -2,10 +2,15 @@ import DB from "../Services/DB.js"
 import { v4 as uuidv4 } from 'uuid';
 
 
-// Each of these methods in this class makes use of the DB.run method 
+// Each of the methods in Package class makes use of the DB.run method 
 // from the DB class to execute our Neo4j Cypher commands.
 export default class Package
 {
+    /**
+     * Creates a new node in the database representing a package with the specified name.
+     * @param {string} name - The name of the package node to create.
+     * @returns {Promise<Object>} - A promise that resolves with the details of the created node.
+    */
     static async createNode(name)
     {
         return DB.run(
@@ -14,6 +19,10 @@ export default class Package
         )
     }
 
+    /**
+     * Deletes all nodes labeled as 'Package' and their associated 'DEPENDS_ON' relationships from the database.
+     * @returns {Promise<Object>} - A promise that resolves when the nodes and relationships are deleted.
+    */
     static async deleteAllNodesAndLinks() {
         return DB.run(
             `
@@ -23,6 +32,12 @@ export default class Package
         );
     }
 
+    /**
+     * Creates a 'DEPENDS_ON' relationship between two existing package nodes identified by their names.
+     * @param {string} parentName - The name of the parent package node.
+     * @param {string} childName - The name of the child package node.
+     * @returns {Promise<Object>} - A promise that resolves with the details of the linked nodes.
+    */
     static async linkNodes(parentName, childName) {
         return DB.run(
             `
