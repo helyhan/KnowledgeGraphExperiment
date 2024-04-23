ee6fb75e8500b65721b409437724aaba9c9b1a17
Hannah Hely <97140666+helyhan@users.noreply.github.com>
2024-04-23 11:16:28 +1000

    Update File.js
    updated comments

--- KnowledgeGraphFinalExperiment/Services/File.js
+++ KnowledgeGraphFinalExperiment/Services/File.js
@@ -1,7 +1,17 @@
 import { readFile } from 'fs/promises';
 
+/**
+ * This class provides a static method to read and parse JSON files from a specified directory.
+*/
 export default class File
 {
+    /**
+     * Reads and parses the first JSON object from a file in the 'data/python-dep' directory.
+     * @param {string} fileName - The name of the file to read. The file must be located in the './data/python-dep' directory.
+     * @returns {Promise<Object>} - A promise that resolves with the first JSON object from the file.
+     * If the file does not exist, cannot be read, or contains invalid JSON, the method logs the error and rethrows it.
+     * This allows for handling specific errors where the method is called.
+    */
     static async parse(fileName)
     {
         try {
